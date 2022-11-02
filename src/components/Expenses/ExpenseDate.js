4d00eca05a074f0ae94614bf95a8becec252bd2f
sahilkadiyan <kadiyans990@gmail.com>
2022-11-02 23:51:54 +0530

    second commit

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -3,6 +3,8 @@ function ExpenseDate(props) {
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const year = props.date.getFullYear();
+    //jfbeugbsbeugubs
+    
 
     return (
         <div className='expense-date'>
