12beeca52da915f7b9f5cb400e547c7360934512
sahilkadiyan <kadiyans990@gmail.com>
2022-11-03 10:29:33 +0530

    title updated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,14 +6,16 @@ function ExpensItem(props) {
     // const expenseDate = new Date(2022, 11, 2);
     // const expenseTitle = 'Car Insurance';
     // const expenseAmount = 294.67
+    let title=props.title;
    const clickHandler=()=>{
-    console.log('Clicked');
+    title="updated";
+    console.log(title);
    }
     return (
         <Card className="expense-item">
            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change title</button>
